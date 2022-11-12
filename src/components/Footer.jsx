6f33ea81a6117aa1c99792da57bf8b44da99a2e0
Miroslav Jovic <jovicmiroslav88@gmail.com>
2022-11-12 19:59:48 +0100

    server info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,6 +18,18 @@ function Footer() {
         </a>
       </p>
       <p>Copyright &copy; 2022</p>
+      <p className="toActivate">
+        To activate server with data, need db.json on port:5000
+      </p>
+      <p className="toActivate">
+        <a
+          href="https://task-tracker-build-mj.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Go to <b>builded</b> project without server
+        </a>
+      </p>
       <Link to={'/about'}>About</Link>
     </footer>
   );
